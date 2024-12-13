d649f823f819c60f6ee73e0f978cbfc1cef945d8
dylanchristensen <r.dylan.christensen@gmail.com>
2024-12-12 23:27:17 -0500

    Update Homepage.js

--- src/Homepage/Homepage.js
+++ src/Homepage/Homepage.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Bar } from 'react-chartjs-2';
-import * as d3 from 'd3'; // Imported as per requirements
+import * as d3 from 'd3'; 
 import '../App.scss';
 
 function Homepage() {
